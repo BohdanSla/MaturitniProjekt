eb834a4cd7e78ac822345c43d27f937084188805
BohdanSla <bohdanslawisch00@gmail.com>
2023-10-19 11:34:25 +0200

    dodelat sidebar + hotovy main

--- kod/index.js
+++ kod/index.js
@@ -6,3 +6,16 @@ for (let index = 0; index < plochy.length; index++) {
     let sirka = nadpisy[index] + 80;
     plochy[index].style.width = sirka + "px";
 }
+
+let ikonaMenu = document.querySelector("header nav img:first-child");
+ikonaMenu.addEventListener("click",otevriPostraniMenu);
+
+let postrannniMenu = document.querySelector("aside");
+
+function otevriPostraniMenu() {
+    if (getComputedStyle(postrannniMenu).getPropertyValue("left") == "-240px") {
+        postrannniMenu.style.left = "0";
+    } else  {
+        postrannniMenu.style.left = "-240px";
+    }
+}
