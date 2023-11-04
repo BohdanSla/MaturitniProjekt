5bce32db406492c12fea88d626b20b4cb18904db
BohdanSla <bohdanslawisch00@gmail.com>
2023-11-04 10:31:14 +0100

    rozpracovana resopnzivita, zatim dokonceny navrh databaze

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 $html = file_get_contents("kod/index.html");
 
 
-while (mb_strpos($html,"[@recenze]") != false) {
-    # code...
-    $html = preg_replace("/\[@recenze\]/",strval(random_int(2,12)), $html,1);
-}
+// while (mb_strpos($html,"[@recenze]") != false) {
+//     # code...
+//     $html = preg_replace("/\[@recenze\]/",strval(random_int(2,12)), $html,1);
+// }
 
 echo $html;
