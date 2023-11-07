3136cd322315a699d485704477e445cf9cc8d862
BohdanSla <bohdanslawisch00@gmail.com>
2023-11-07 10:51:04 +0100

    hotove postranni menu

--- kod/index.js
+++ kod/index.js
@@ -13,13 +13,53 @@ ikonaMenu.addEventListener("click",otevriPostraniMenu);
 let postranniMenu = document.querySelector(".kategorie");
 let tmavaPlocha = document.querySelector("aside");
 
+if (window.innerWidth > 650) {
+    postranniMenu.style.left = "-240px";
+} else {
+    postranniMenu.style.top = "-320px";
+}
+
+
+window.addEventListener("resize",zmenPolohuPostrannihoMenu)
+
+function zmenPolohuPostrannihoMenu() {
+    console.log(this.innerWidth);
+    if (this.innerWidth > 650) {
+        if (tmavaPlocha.style.visibility == "visible") {
+            postranniMenu.style.left = "0";
+        } else {
+            postranniMenu.style.top = "0";
+            postranniMenu.style.left = "-240px";
+        }
+    } else {
+        if (tmavaPlocha.style.visibility == "visible") {
+            postranniMenu.style.top = "0";
+        } else {
+            postranniMenu.style.left = "0";
+            postranniMenu.style.top = "-320px";
+        }
+    }
+}
+
+let prvni = true
+
 function otevriPostraniMenu() {
-    if (getComputedStyle(postranniMenu).getPropertyValue("left") == "-240px") {
-        postranniMenu.style.left = "0";
-        tmavaPlocha.style.visibility = "visible";
-    } else  {
-        postranniMenu.style.left = "-240px";
-        tmavaPlocha.style.visibility = "hidden";
+    if (window.innerWidth > "650") {
+        if (getComputedStyle(postranniMenu).getPropertyValue("left") == "-240px") {
+            postranniMenu.style.left = "0";
+            tmavaPlocha.style.visibility = "visible";
+        } else  {
+            postranniMenu.style.left = "-240px";
+            tmavaPlocha.style.visibility = "hidden";
+        }
+    } else {
+        if (getComputedStyle(postranniMenu).getPropertyValue("top") == "0px") {
+            postranniMenu.style.top = "-320px";
+            tmavaPlocha.style.visibility = "hidden";
+        } else  {
+            postranniMenu.style.top = "0";
+            tmavaPlocha.style.visibility = "visible";
+        }
     }
 }
 let polozkaSlideru = 0
@@ -52,7 +92,6 @@ function predchoziDoporucenyProdukt() {
 
     }
     doporucenyProdukty[polozkaSlideru].style.display ="flex"
-    console.log(polozkaSlideru);
 }
 
 
