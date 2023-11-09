54e763452792d8f75c89572838c73f75a1bb61fd
BohdanSla <bohdanslawisch00@gmail.com>
2023-11-09 17:43:01 +0100

    dokonceni ERD + zacatek db + php

---
+++ hodnoty.sql
@@ -0,0 +1,17 @@
+INSERT into produkt(nazev,popis,cena,cena_ve_sleve,hodnoceni_produktu,id_znacky,id_sportu,id_typu_produktu) VALUES ("EASTRAIL 2 R.RDY","Dopřejte si na trailu pohodlí. Tyto turistické boty adidas EASTRAIL 2 R.RDY vás v mokrém a promáčeném prostředí udrží v suchu a pohodlí díky technologii RAIN.RDY. Přilnavá podešev Traxion vám na kluzkém a nerovném povrchu dopřeje naprostou jistotu. A to nejlepší? Nepotřebují rozchodit, takže se v nich budete moci vydat rovnou na túru.",1899,NULL,(SELECT COALESCE(AVG(recenze.pocet_hvezd),0) FROM recenze),1,1,1)
+
+INSERT INTO recenze(recenze, pocet_hvezd, id_produktu, id_uzivatele) VALUES ("Vyzerajú dobre, ale vrátené, lebo nesedela veľkosť, napriek tomu, že mám tejto veľkosti už dvojo adidasov. Objednané väčšie...",3,1,2)
+
+SELECT produkt.nazev, produkt.popis, produkt.cena, produkt.hodnoceni_produktu, obrazek.obrazek_src
+FROM produkt 
+JOIN obrazky_k_produktu ON produkt.id = obrazky_k_produktu.id_produktu
+JOIN obrazek ON obrazky_k_produktu.id_obrazku = obrazek.id
+LIMIT 4
+
+UPDATE produkt JOIN (
+	SELECT AVG(recenze.pocet_hvezd) AS prumer
+    FROM recenze 
+    GROUP BY recenze.id_produktu
+    HAVING recenze.id_produktu = 1
+) subquery ON 1=1
+SET produkt.hodnoceni_produktu = subquery.prumer
--- index.php
+++ index.php
@@ -2,12 +2,43 @@
 
 declare(strict_types=1);
 
-$html = file_get_contents("kod/index.html");
+$html = file_get_contents("kod/stranky/index.html");
+
+$db = new PDO("mysql:host=localhost;dbname=pro_sportovce;charset=utf8","root","");
+
+$stmt = $db->prepare("SELECT produkt.nazev, produkt.popis, produkt.cena, produkt.hodnoceni_produktu, obrazek.obrazek_src
+FROM produkt 
+JOIN obrazky_k_produktu ON produkt.id = obrazky_k_produktu.id_produktu
+JOIN obrazek ON obrazky_k_produktu.id_obrazku = obrazek.id
+LIMIT 4");
+
+$stmt->execute();
+$arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($arr as $key => $value) {
+    # code...
+    $zformulovanaCena = "";
+
+    $cena = strval($value["cena"]);
+    $delka = mb_strlen($cena);
+
+    for ($i=$delka - 1; $i >= 0; $i--) { 
+        # code...
+        $zformulovanaCena = $cena[$i] . $zformulovanaCena;
+        if (($delka - $i) % 3 == 0 && $i > 0) {
+            $zformulovanaCena = " " . $zformulovanaCena;
+        }
+    }
+
+    $src = "kod/obrazky/" . $value["obrazek_src"];
+    $cena = $zformulovanaCena . " Kč";
 
 
-// while (mb_strpos($html,"[@recenze]") != false) {
-//     # code...
-//     $html = preg_replace("/\[@recenze\]/",strval(random_int(2,12)), $html,1);
-// }
+    $html = preg_replace("/\[@doporuceny-produkt-obrazek]/",$src, $html,1);
+    $html = preg_replace("/\[@nazev\]/",$value["nazev"], $html,1);
+    $html = preg_replace("/\[@cena\]/",$cena, $html,1);
+    $html = preg_replace("/\[@recenze\]/",strval($value["hodnoceni_produktu"]), $html,1);
+    $html = preg_replace("/\[@popis\]/",strval($value["popis"]), $html,1);
+}
 
 echo $html;
