68208b4d711a9c45ed935f8be8c3d731872467cf
BohdanSla <bohdanslawisch00@gmail.com>
2023-11-14 11:32:46 +0100

    predelani html,css + pridavani produktu

--- index.php
+++ index.php
@@ -17,9 +17,41 @@ $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($arr as $key => $value) {
     # code...
-    $zformulovanaCena = "";
 
-    $cena = strval($value["cena"]);
+    $src = "kod/obrazky/" . $value["obrazek_src"];
+
+    $html = preg_replace("/\[@doporuceny-produkt-obrazek]/",$src, $html,1);
+    $html = preg_replace("/\[@nazev\]/",$value["nazev"], $html,1);
+    $html = preg_replace("/\[@cena\]/",zformulujCenu(strval($value["cena"])), $html,1);
+    $html = preg_replace("/\[@recenze\]/",strval($value["hodnoceni_produktu"]), $html,1);
+    $html = preg_replace("/\[@popis\]/",strval($value["popis"]), $html,1);
+}
+
+
+$stmt = $db->prepare("SELECT produkt.nazev, znacka.znacka, produkt.cena, produkt.cena_ve_sleve, obrazek.obrazek_src 
+FROM produkt, znacka, obrazek 
+JOIN obrazky_k_produktu ON obrazky_k_produktu.id_obrazku = obrazek.id 
+WHERE znacka.id = produkt.id_znacky 
+AND produkt.id = obrazky_k_produktu.id_produktu 
+AND produkt.cena_ve_sleve IS NOT NULL;");
+
+$stmt->execute();
+$arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($arr as $key => $value) {
+    # code...
+
+    $src = "kod/obrazky/" . $value["obrazek_src"];
+
+    $html = preg_replace("/\[@produkt-ve-sleve-obrazek]/",$src, $html,1);
+    $html = preg_replace("/\[@produkt-ve-sleve-nazev\]/",$value["nazev"], $html,1);
+    $html = preg_replace("/\[@produkt-ve-sleve-znacka\]/",strval($value["znacka"]), $html,1);
+    $html = preg_replace("/\[@produkt-ve-sleve-cena\]/",zformulujCenu(strval($value["cena"])), $html,1);
+    $html = preg_replace("/\[@produkt-ve-sleve-cena_ve_sleve\]/",zformulujCenu(strval($value["cena_ve_sleve"])), $html,1);
+}
+function zformulujCenu(string $cena): string {
+
+    $zformulovanaCena = "";
     $delka = mb_strlen($cena);
 
     for ($i=$delka - 1; $i >= 0; $i--) { 
@@ -29,16 +61,7 @@ foreach ($arr as $key => $value) {
             $zformulovanaCena = " " . $zformulovanaCena;
         }
     }
-
-    $src = "kod/obrazky/" . $value["obrazek_src"];
-    $cena = $zformulovanaCena . " Kč";
-
-
-    $html = preg_replace("/\[@doporuceny-produkt-obrazek]/",$src, $html,1);
-    $html = preg_replace("/\[@nazev\]/",$value["nazev"], $html,1);
-    $html = preg_replace("/\[@cena\]/",$cena, $html,1);
-    $html = preg_replace("/\[@recenze\]/",strval($value["hodnoceni_produktu"]), $html,1);
-    $html = preg_replace("/\[@popis\]/",strval($value["popis"]), $html,1);
+    return $cena = $zformulovanaCena . " Kč";
 }
 
 echo $html;
--- vypis.sql
+++ vypis.sql
@@ -1,13 +1,23 @@
+/*hodnoty do databaze*/
+/*muzu dat export z phpmyadmin nebo extensionu!!!!!!!!!!!!!!!!!!!!!*/
 INSERT into produkt(nazev,popis,cena,cena_ve_sleve,hodnoceni_produktu,id_znacky,id_sportu,id_typu_produktu) VALUES ("EASTRAIL 2 R.RDY","Dopřejte si na trailu pohodlí. Tyto turistické boty adidas EASTRAIL 2 R.RDY vás v mokrém a promáčeném prostředí udrží v suchu a pohodlí díky technologii RAIN.RDY. Přilnavá podešev Traxion vám na kluzkém a nerovném povrchu dopřeje naprostou jistotu. A to nejlepší? Nepotřebují rozchodit, takže se v nich budete moci vydat rovnou na túru.",1899,NULL,(SELECT COALESCE(AVG(recenze.pocet_hvezd),0) FROM recenze),1,1,1)
 
 INSERT INTO recenze(recenze, pocet_hvezd, id_produktu, id_uzivatele) VALUES ("Vyzerajú dobre, ale vrátené, lebo nesedela veľkosť, napriek tomu, že mám tejto veľkosti už dvojo adidasov. Objednané väčšie...",3,1,2)
-
+/*vyber produktu*/
 SELECT produkt.nazev, produkt.popis, produkt.cena, produkt.hodnoceni_produktu, obrazek.obrazek_src
 FROM produkt 
 JOIN obrazky_k_produktu ON produkt.id = obrazky_k_produktu.id_produktu
 JOIN obrazek ON obrazky_k_produktu.id_obrazku = obrazek.id
 LIMIT 4
 
+SELECT produkt.nazev, znacka.znacka, produkt.cena, produkt.cena_ve_sleve, obrazek.obrazek_src 
+FROM produkt, znacka, obrazek 
+JOIN obrazky_k_produktu ON obrazky_k_produktu.id_obrazku = obrazek.id 
+WHERE znacka.id = produkt.id_znacky 
+AND produkt.id = obrazky_k_produktu.id_produktu 
+AND produkt.cena_ve_sleve IS NOT NULL;
+
+/*uprava databaze*/
 UPDATE produkt JOIN (
 	SELECT AVG(recenze.pocet_hvezd) AS prumer
     FROM recenze 
